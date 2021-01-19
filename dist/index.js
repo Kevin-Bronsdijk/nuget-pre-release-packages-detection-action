75461d9b46bc69156becf98f96547c7126b676ab
Kevin Brondijk <kevin.bronsdijk@agoda.com>
2021-01-19 09:52:07 +0700

    testing

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@ module.exports =
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 938:
+/***/ 866:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __nccwpck_require__) => {
 
 const core = __nccwpck_require__(644);
@@ -5941,6 +5941,6 @@ module.exports = require("zlib");;
 /******/ 	// module exports must be returned from runtime so entry inlining is disabled
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	return __nccwpck_require__(938);
+/******/ 	return __nccwpck_require__(866);
 /******/ })()
 ;
